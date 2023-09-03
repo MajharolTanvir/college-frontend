f9b9f56be2a4e1f96a8fd28a7c8fddb14d7da664
MajharolTanvir <majharul5456@gmail.com>
2023-09-03 18:58:10 +0600

    Start the work

---
+++ src/components/Layouts/RootLayouts.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const RootLayouts = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default RootLayouts;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="light">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
