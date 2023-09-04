df314454327f49945722f2eda72d8a323eda3c7d
Fahad <205fahadmohammad@gmail.com>
2023-09-04 17:27:58 +0600

    introduction updated

--- src/components/UI/messageCard/MessageCard.tsx
+++ src/components/UI/messageCard/MessageCard.tsx
@@ -1,35 +1,40 @@
+import Image from "next/image";
 import React from "react";
 
 const MessageCard = () => {
   return (
     <section className="container mx-auto flex justify-evenly">
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl flex">
-          <div>
-            <figure className="px-10 pt-10">
-              <img src="/principal.jpg" alt="Shoes" className="rounded-xl" />
-            </figure>
+        <div className="card w-full bg-base-100 shadow-xl grid grid-cols-2 gap-x-5">
+          <div className="pl-10 pt-10">
+            <Image
+              src="/principal.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+              width={200}
+              height={200}
+            />
           </div>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">Message of the Principal</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
       </div>
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-            <img src="/vice-principal.jpg" alt="Shoes" className="rounded-xl" />
-          </figure>
+        <div className="card w-fit bg-base-100 shadow-xl grid grid-cols-2">
+          <div className="pl-10 pt-10">
+            <Image
+              src="/vice-principal.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+              width={200}
+              height={200}
+            />
+          </div>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Shoes!</h2>
+            <h2 className="card-title">Message of the Vice-Principal</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
       </div>
