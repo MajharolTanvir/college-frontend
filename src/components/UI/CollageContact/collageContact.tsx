33b02b6cdce464c1cae95ed65c021b7bc0a40f44
MajharolTanvir <majharul5456@gmail.com>
2023-09-04 21:05:51 +0600

    Name design updated

--- src/components/UI/CollageContact/collageContact.tsx
+++ src/components/UI/CollageContact/collageContact.tsx
@@ -2,14 +2,14 @@
 
 const CollageContact = () => {
   return (
-    <div className="bg-green-100 hidden md:block">
-      <div className="lg:container lg:mx-auto text-[#262582]  py-3  md:mx-5">
-        <div className="md:flex justify-between">
+    <div className="bg-[#262582] hidden md:block">
+      <div className="lg:container lg:mx-auto py-5  md:mx-5">
+        <div className="md:flex justify-between text-white">
           <h3 className="text=[5px] md:text-xl font-medium">
             Email: publiccollege@gmail.com
           </h3>
           <h3 className="text=[5px] md:text-xl font-medium">
-            Contact: 018++++++++
+            Contact: +8801843098127
           </h3>
           <h3 className="text=[5px] md:text-xl font-medium">EIIN: 136979</h3>
         </div>
--- src/components/UI/collegeName/collegeName.tsx
+++ src/components/UI/collegeName/collegeName.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const CollegeName = () => {
-    return (
-      <div className="text-xl md:text-4xl lg:text-5xl py-4 font-bold flex justify-center items-center ">
-        <h1>RANGAMATI PUBLIC COLLEGE</h1>
-      </div>
-    );
+  return (
+    <div className="text-xl md:text-4xl lg:text-5xl py-4 font-bold flex justify-center items-center relative content bg-gradient-to-r from-[#262582] via-[#7372bb] to-[#262582] text-white">
+      <h1>RANGAMATI PUBLIC COLLEGE</h1>
+    </div>
+  );
 };
 
 export default CollegeName;
